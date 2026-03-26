7454d2949be55e10cc42db2925bff7d03223f7f9
hectorsanmiguel <hector.sanmiguel@alumnos.uneatlantico.es>
2026-03-26 12:42:33 +0100

    Vista privada del constructor

--- entregas/sanmiguelHector/src/Vehiculo.java
+++ entregas/sanmiguelHector/src/Vehiculo.java
@@ -1,7 +1,14 @@
 class Vehiculo {
 
+    private String matricula;
+    private String modelo;
+    private int tipoVehiculo;
 
-    public Vehiculo(String matricula, int tipoPlaza, String modelo){}
+    public Vehiculo(String matricula, int tipoVehiculo, String modelo){
+        this.matricula = matricula;
+        this.modelo = modelo;
+        this.tipoVehiculo = tipoVehiculo;
+    }
 
     public void mostrarDatos(){}
 
