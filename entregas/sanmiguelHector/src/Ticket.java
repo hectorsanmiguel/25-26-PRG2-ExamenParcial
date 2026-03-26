2ee294241007cb9157302e912a8ab68a473db2af
hectorsanmiguel <hector.sanmiguel@alumnos.uneatlantico.es>
2026-03-26 13:08:09 +0100

    Vista pública de la clase Ticket

---
+++ entregas/sanmiguelHector/src/Ticket.java
@@ -0,0 +1,13 @@
+class Ticket {
+    
+
+    public Ticket(int horaLLegada, int horaSalida, Plaza plaza, Vehiculo vehiculo){}
+
+
+    public int precioTotalEstancia(){}
+
+    public int horasAparcado(){}
+
+    public void mostrarTicket(){}
+    
+}
