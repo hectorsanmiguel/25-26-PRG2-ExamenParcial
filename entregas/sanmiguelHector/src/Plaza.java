3d9be6cd451ce2e8bfe5992373b759cc828d1435
hectorsanmiguel <hector.sanmiguel@alumnos.uneatlantico.es>
2026-03-26 12:24:06 +0100

    Vista pública de Plaza

---
+++ entregas/sanmiguelHector/src/Plaza.java
@@ -0,0 +1,13 @@
+class Plaza {
+    
+    public Plaza (int tipoPlaza, boolean ocupada){}
+
+    public boolean estaOcupada(){}
+
+    public int precioPlaza(){}
+
+    public String quienOcupaPlaza(){}
+
+    public void mostrarDatos(){}
+
+}
